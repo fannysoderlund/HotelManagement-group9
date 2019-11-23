5d1b437ec5ffb368b6470ca65a077ef74ee9324f
Fanny <fannysarasoderlund@gmail.com>
2019-11-23 22:45:26 +0100

    changed the sign in method

--- fanny/Main.java
+++ fanny/Main.java
@@ -1,22 +1,36 @@
 package fanny;
+
 import java.util.Scanner;
+
 public class Main {
-Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Main myApp=new Main();
+        Main myApp = new Main();
         String user = myApp.signIn();
-        if (user.contains(".hotel")) {
-            user = "Employee";
-            //call menu employee
-        }else {
-            user= "Customer";
-            //call menu customer
-        }
+        //use the string user to call correct menu
     }
 
     public String signIn() {
-        System.out.println("Please log in\nIf you are a customer, sign in with your SSN(YYMMDDXXX)\n" +
-                "If you are an employee, sign in with your login.hotel\n");
-        return input.nextLine();
+        String user;
+        while (true) {
+            try {
+                System.out.println("Please log in\n.............\nIf you are a customer, sign in with your SSN (YYMMDDXXXX)\n" +
+                        "If you are an employee, sign in with your login.hotel\n");
+                user = input.nextLine();
+                if (user.contains(".hotel")) {
+                    user = "Employee";
+                    break;
+                } else if (user.toCharArray().length == 10 && (user.matches("[0-9]+"))) {
+                    user = "Customer";
+                    break;
+                } else {
+                    System.out.println("Please enter a correct log in\n");
+                }
+            } catch (Exception e) {
+                System.out.println("Please enter a correct log in\n");
+            }
     }
+        return user;
+}
 }
